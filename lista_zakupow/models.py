afb3d0b78d92a31b0ef0c15d0dfd03f297c012b9
CarlosC602 <karolostapinski9@gmail.com>
2025-04-28 22:12:15 +0200

    Update models.py
    Dodanie klasy Shopping list

--- lista_zakupow/models.py
+++ lista_zakupow/models.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 
@@ -6,3 +5,8 @@ class Product:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+class ShoppingList:
+    def __init__(self):
+        self.lists = {}
+        self.current_theme = "light"
+        self.load_data()
