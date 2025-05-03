3925979fc0db8e420612140d3fcb90808a9feb24
Krzysztof Olszewski <krzysztofjuzolszewski@gmail.com>
2025-05-03 22:34:13 +0200

    Update theme_manager.py
    poprawione GUI;
    dodany tryb nocny

--- lista_zakupow/theme_manager.py
+++ lista_zakupow/theme_manager.py
@@ -1,8 +1,10 @@
 class ThemeManager:
     @staticmethod
     def get_themes():
-        return {
-            "light": {
+        return 
+        {
+            "light": 
+            {
                 "bg": "#FFFFFF",
                 "fg": "#2d3436",
                 "secondary_bg": "#f5f6fa",
@@ -15,4 +17,18 @@ class ThemeManager:
                 "header_bg": "#0984e3",
                 "header_fg": "#ffffff"
             },
+            "dark":
+            {
+                    "bg": "#2d3436",
+                    "fg": "#dfe6e9",
+                    "secondary_bg": "#34495e",
+                    "accent": "#3498db",
+                    "text": "#dfe6e9",
+                    "widget_bg": "#34495e",
+                    "hover": "#2980b9",
+                    "tree_bg": "#34495e",
+                    "tree_fg": "#dfe6e9",
+                    "header_bg": "#2c3e50",
+                    "header_fg": "#dfe6e9"
+            }
         }
